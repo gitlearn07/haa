19f79022333481493a178b4296085d8b4f362838
gitlearn07 <rami.412222@yahoo.com>
2021-06-13 17:48:40 +0200

    initial commit

---
+++ src/main/java/com/kg/kgmini/KgminiApplication.java
@@ -0,0 +1,14 @@
+package com.kg.kgmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgminiApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/kg/kgmini/KgminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kg.kgmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
